1746da929e11a59b769b5bfbd2362738ed173674
silvershine157 <speedstar157@gmail.com>
2017-11-15 17:07:20 +0900

    big picture

--- hw2.py
+++ hw2.py
@@ -1,1 +1,28 @@
 import numpy as np
+import urllib.request as rq
+
+def main():
+    #get data
+    #make 5-fold partition
+
+    #Problem 1
+    #for different learning rates
+    #   graph1.append train logistic
+    #   error1.append test logistic
+
+    #Problem 2
+    #for different C
+    #   avgerrors2.append cross validation SVM linear kernel
+    #error2 = test SVM linear kernel for best C
+
+    #Problem 3
+    #for different C
+    #   for dfiferent sigma
+    #       avgerrors3.append cross validation SVM gaussian kernel
+    #error3 = test SVM gaussian kernel for best C and sigma
+
+    # report errors
+    # draw graphs
+    pass
+
+main()
