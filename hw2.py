eaeab2a97ced26b6e0fb7691b734ed974259202a
silvershine157 <speedstar157@gmail.com>
2017-11-16 04:14:58 +0900

    logistic regression

--- hw2.py
+++ hw2.py
@@ -9,6 +9,8 @@ def main():
     #for different learning rates
     #   graph1.append train logistic
     #   error1.append test logistic
+    logistic_beta, logistic_history = train_logistic(trainX, trainY, 0.0009, 500)
+    logistic_error = test_logistic(testX, testY, logistic_beta)
 
     #Problem 2
     #for different C
@@ -24,6 +26,32 @@ def main():
     # report errors
     # draw graphs
     pass
+
+# logistic regression by gradient descent
+def train_logistic(trainX, trainY, step_size, iterations):
+    history = []
+    beta = np.full(trainX.shape[1], 0);
+    for i in range(iterations):
+        history.append(loss_logistic(trainX, trainY, beta))
+        beta = beta - step_size*grad_logistic(trainX, trainY, beta)
+    return beta, history
+
+def loss_logistic(trainX, trainY, beta):
+    P = 1/(1+np.exp(-np.dot(trainX, beta)))
+    loss = np.sum(-trainY*np.log(P) - (1-trainY)*np.log(1-P))
+    return loss
+
+# gradient of logistic loss
+def grad_logistic(trainX, trainY, beta):
+    P = 1/(1+np.exp(-np.dot(trainX, beta)))
+    grad = -np.dot(trainY-P, trainX)
+    return grad
+
+def test_logistic(testX, testY, beta):
+    decision = np.dot(testX, beta) > 0
+    num_correct = np.sum(decision == testY)
+    pred_error = 1 - (num_correct/testX.shape[0])
+    return pred_error
 
 # fetch traning / test data
 def get_data():
@@ -58,7 +86,7 @@ def get_data():
 def parse_data_line(line):
     word_list = line.rstrip().split()
     label = int(word_list[0])
-    ZERO_ONES = False;
+    ZERO_ONES = True;
     if(ZERO_ONES and label == -1):
         label = 0
     encode = []
@@ -83,3 +111,6 @@ def k_partition(trainX, trainY, k):
     partY = trainY[(k-1)*b_size:]
     partition.append((partX, partY))
     return partition
+
+
+main()
